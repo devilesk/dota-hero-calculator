c05d4f4cd3a1af634bc475e086966054320b5d4d
devilesk <devilesk@gmail.com>
2016-07-28 13:21:44 -0400

    add rollbar

--- www/js/main.js
+++ www/js/main.js
@@ -11,7 +11,8 @@ require.config({
         'chartjs-scatter': 'lib/Chart.scatter',
         'text': 'lib/text',
         'herocalc_knockout': 'herocalc/herocalc_knockout',
-        'jquery-ui.custom': 'herocalc/jquery-ui.custom'
+        'jquery-ui.custom': 'herocalc/jquery-ui.custom',
+        'rollbar': 'lib/rollbar.umd.nojson.min'
     },
     shim: {
         'bootstrap': {
@@ -20,56 +21,67 @@ require.config({
     }
 });
 
-require(['jquery', 'herocalc'], function ($, HEROCALCULATOR) {
-    $('.top-nav-menu .dropdown-toggle').click(function() {
-      if ($('.mobile-only').css('display') == 'none') {
-        var location = $(this).attr('href');
-        window.location.href = location;
-        return false;
-      }
-    });
-    var cssPath = "css/hero-calculator.theme";
-    if (readCookie('theme') == 'light') {
-        $('#theme-css').attr('href','/media/css/site-light.css');
-        $('#hero-css').attr('href', cssPath + '.light.css');
-        $("#theme-select").val('light');
-    }
-    else {
-        $('#theme-css').attr('href','/media/css/site.css');
-        $('#hero-css').attr('href', cssPath + '.dark.css');
-        $("#theme-select").val('dark');
-    }
-    $('#theme-select').change( function() {
-        var expiration_date = 365*24*60*60*1000;
-        if ($(this).val() == 'light') {
+var rollbarConfig = {
+  accessToken: 'de1980fcab4849d6a7a066cf098a6521',
+  captureUncaught: true,
+  payload: {
+    environment: 'development',
+  }
+};
+
+require(['rollbar'], function (Rollbar) {
+    var rollbar = Rollbar.init(rollbarConfig);
+    require(['jquery', 'herocalc'], function ($, HEROCALCULATOR) {
+        $('.top-nav-menu .dropdown-toggle').click(function() {
+          if ($('.mobile-only').css('display') == 'none') {
+            var location = $(this).attr('href');
+            window.location.href = location;
+            return false;
+          }
+        });
+        var cssPath = "css/hero-calculator.theme";
+        if (readCookie('theme') == 'light') {
             $('#theme-css').attr('href','/media/css/site-light.css');
             $('#hero-css').attr('href', cssPath + '.light.css');
-            var d = new Date();
-            d.setTime(d.getTime() + expiration_date);
-            document.cookie = 'theme=light;path=/;expires='+d.toGMTString()+';max-age='+expiration_date+';';
+            $("#theme-select").val('light');
         }
         else {
             $('#theme-css').attr('href','/media/css/site.css');
             $('#hero-css').attr('href', cssPath + '.dark.css');
-            var d = new Date();
-            d.setTime(d.getTime() + expiration_date);
-            document.cookie = 'theme=dark;path=/;expires='+d.toGMTString()+';max-age='+expiration_date+';';
+            $("#theme-select").val('dark');
         }
+        $('#theme-select').change( function() {
+            var expiration_date = 365*24*60*60*1000;
+            if ($(this).val() == 'light') {
+                $('#theme-css').attr('href','/media/css/site-light.css');
+                $('#hero-css').attr('href', cssPath + '.light.css');
+                var d = new Date();
+                d.setTime(d.getTime() + expiration_date);
+                document.cookie = 'theme=light;path=/;expires='+d.toGMTString()+';max-age='+expiration_date+';';
+            }
+            else {
+                $('#theme-css').attr('href','/media/css/site.css');
+                $('#hero-css').attr('href', cssPath + '.dark.css');
+                var d = new Date();
+                d.setTime(d.getTime() + expiration_date);
+                document.cookie = 'theme=dark;path=/;expires='+d.toGMTString()+';max-age='+expiration_date+';';
+            }
+        });
+        function readCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+        
+        hc = new HEROCALCULATOR.HEROCALCULATOR();
+        var lastUpdate = "#Development";
+        $('#last-update').text(lastUpdate);
+        
+        hc.init("/media/js/herodata.json","/media/js/itemdata.json","/media/js/unitdata.json");
     });
-    function readCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-    
-    hc = new HEROCALCULATOR.HEROCALCULATOR();
-    var lastUpdate = "#Development";
-    $('#last-update').text(lastUpdate);
-    
-    hc.init("/media/js/herodata.json","/media/js/itemdata.json","/media/js/unitdata.json");
 });
