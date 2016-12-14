840ce1c447c92350031a575cdb77d1d8d86fcbaf
devilesk <devilesk@gmail.com>
2016-12-14 08:32:52 -0500

    add startsWith polyfill

--- src/js/app/polyfill.js
+++ src/js/app/polyfill.js
@@ -21,3 +21,9 @@ if (!Array.prototype.find) {
     return undefined;
   };
 }
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
